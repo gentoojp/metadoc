a79a587cc0d5a5d257a0212997e2352a920f0f78
Kazunori Kojima <dk@at-dk.info>
2011-09-24 17:12:00 +0900

    tmp

--- handbook.py
+++ handbook.py
@@ -13,6 +13,10 @@ class Handbook(SimpleDoc):
         self.__set_docinfo()
 
     def __set_docinfo(self):
+        if not self.meta_info['en_memberof']:
+            self.meta_info['en_memberof'] = {'Handbook': 'Handbook'}
+        else:
+            self.meta_info['en_memberof']['Handbook'] = 'Handbook'
         # Parse inculde document
         try:
             tree = etree.parse(CONFIG['BASE_PATH'] + self.meta_info['file_en_path'])
@@ -23,8 +27,8 @@ class Handbook(SimpleDoc):
                 meta_info['file_en_path'] = os.path.dirname(self.meta_info['file_en_path']) + '/' + i.get('href')
                 meta_info['file_id'] = i.get('href').replace('.xml', '')
                 meta_info['parent'] = self.meta_info['file_id']
-                meta_info['en_memberof'] = None
-                meta_info['ja_memberof'] = None
+                meta_info['en_memberof'] = 'Handbook'
+                meta_info['ja_memberof'] = 'Handbook'
                 file_ja_path = meta_info['file_en_path'].replace('/en/', '/ja/')
                 if file_ja_path == os.path.exists(CONFIG['BASE_PATH'] + file_ja_path):
                     meta_info['file_ja_path'] = file_ja_path
