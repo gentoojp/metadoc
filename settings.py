d59d74af51618930b59324a87c339303539aea95
Kazunori Kojima <dk@at-dk.info>
2011-09-24 17:11:28 +0900

    tmp

--- settings.py
+++ settings.py
@@ -6,14 +6,15 @@ CONFIG['metadoc_en'] = "/doc/en/metadoc.xml"
 CONFIG['metadoc_ja'] = "/doc/ja/metadoc.xml"
 
 CONFIG['BASE_PATH'] =  "/usr/local/jpdoc/cvs/gentoo"
+CONFIG['BASE_PATH'] =  "/home/kazunori/repos/gentoojp/gentoo-docs/xml/htdocs/"
 CONFIG['HANDBOOK_DIR'] = ('handbook', 'security')
 
 CONFIG['NON_LIST_FILE'] = ('metadoc','metadoc-index', 'metadoc-list', 'metadoc-overview', 'doc-languages', 'inserts')
 
 CONFIG['GENTOO_URL_BASE'] = 'http://www.gentoo.org'
 CONFIG['CVS_URL_BASE'] = 'http://sources.gentoo.org/cgi-bin/viewvc.cgi/gentoo/xml/htdocs'
-CONFIG['DIFF_DIF'] = '/var/www/projects.gentoo.gr.jp/htdocs/docs/diff'
-CONFIG['DIFF_DIR'] = '/tmp/'
+CONFIG['DIFF_DIF'] = u'/var/www/projects.gentoo.gr.jp/htdocs/docs/diff'
+CONFIG['DIFF_DIR'] = u'/tmp/'
 
 CONFIG['BASE_TAMPLATE'] = "./template/trans_status_base.xml"
 CONFIG['CHAPTER_TEMPLATE'] = "./template/trans_status_chapter.xml"
